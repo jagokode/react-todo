e785452834cca532772c72ac2fc1883a64d155ad
jagokode <jagokode@gmail.com>
2020-03-19 20:02:55 +0700

    finish todo input

--- src/App.js
+++ src/App.js
@@ -7,21 +7,31 @@ import TodoList from './components/TodoList';
 
 class App extends Component {
   state = {
-    items: [
-      { id: 1, title: 'Wake Up Early' },
-      { id: 2, title: 'Do some exercises' }
-    ],
+    items: [],
     id: uuidv4(),
     item: '',
     editItem: false
   };
 
   handleChange = e => {
-    console.log('handle change');
+    this.setState({
+      item: e.target.value
+    });
   };
 
   handleSubmit = e => {
-    console.log('handle submit');
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+    const updatedItems = [...this.state.items, newItem];
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id: uuidv4(),
+      editItem: false
+    });
   };
 
   clearList = () => {
