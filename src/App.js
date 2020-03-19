ed3142a0a3abbf72bb5c1f15278ab92c27a0d436
jagokode <jagokode@gmail.com>
2020-03-19 14:20:46 +0700

    Add App Component Structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import uuid from 'uuid';
+import React, { Component } from 'react';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+import { v4 as uuidv4 } from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: 'Wake Up Early' },
+      { id: 2, title: 'Do some exercise' }
+    ],
+    id: uuidv4(),
+    item: '',
+    editItem: false
+  };
+
+  handleChange = e => {
+    console.log('handle change');
+  };
+
+  handleSubmit = e => {
+    console.log('handle submit');
+  };
+
+  clearList = () => {
+    console.log('clear list');
+  };
+
+  handleDelete = id => {
+    console.log(`handle edit ${id}`);
+  };
+
+  handleEdit = id => {
+    console.log(`handle Edit for ${id}`);
+  };
+
+  render() {
+    // console.log(this.state);
+    return (
       <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <h3 className="text-capitalize text-center">todo list</h3>
+            <TodoList
+              items={this.state.items}
+              handleEdit={this.handleEdit}
+              handleDelete={this.handleDelete}
+              clearList={this.clearList}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
